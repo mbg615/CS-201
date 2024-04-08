de4707c4c97e3d61003f2cb6b0f98196d55fc88b
Xoddam <maddox@guthriehouse.rocks>
2024-04-08 08:30:01 -0500

    Initial structure commit

---
+++ Phase 3/BHeap.cpp
@@ -0,0 +1,19 @@
+//
+// Created by Maddox Guthrie on 4/8/24.
+//
+
+#include "BNode.cpp"
+
+template<class T>
+class BHeap {
+private:
+    BNode<T> *minElement; // Pointer to minimum element in heap
+    BNode<T> *heapStart; // Pointer to first element in heap
+
+public:
+
+
+private:
+
+
+};
---
+++ Phase 3/BNode.cpp
@@ -0,0 +1,21 @@
+//
+// Created by Maddox Guthrie on 4/8/24.
+//
+
+#include "BNode.cpp"
+
+template<class T>
+class BNode {
+private:
+    int heapType;
+    BNode<T> *leftNode;
+    BNode<T> *rightNode;
+    BNode<T> *children;
+
+public:
+
+
+private:
+
+
+};
